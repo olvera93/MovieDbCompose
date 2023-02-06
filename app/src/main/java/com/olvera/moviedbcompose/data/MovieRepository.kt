245bf4556ada1ef8cba4257d57a325da34f6fe6f
Olvera <olvera9302@outlook.com>
2023-02-05 20:35:37 -0600

    [ADD] MoviePopular and MovieUpcoming in only screen

--- app/src/main/java/com/olvera/moviedbcompose/data/MovieRepository.kt
+++ app/src/main/java/com/olvera/moviedbcompose/data/MovieRepository.kt
@@ -1,37 +1,53 @@
 package com.olvera.moviedbcompose.data
 
 import com.olvera.moviedbcompose.di.DispatchersModule
-import com.olvera.moviedbcompose.model.Movie
 import com.olvera.moviedbcompose.model.MovieResult
 import com.olvera.moviedbcompose.util.NetworkResult
 import com.olvera.moviedbcompose.util.makeNetworkCall
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 interface MovieTask {
 
-    suspend fun getMoviePopular(
+    suspend fun getMovies(
         apiKey: String
     ): NetworkResult<MovieResult>
-}
 
+}
 
 class MovieRepository @Inject constructor(
     private val movieApi: MovieApi,
     @DispatchersModule.IoDispatcher private val dispatcher: CoroutineDispatcher
-): MovieTask {
-
-    override suspend fun getMoviePopular(apiKey: String): NetworkResult<MovieResult> {
+) : MovieTask {
+    override suspend fun getMovies(apiKey: String): NetworkResult<MovieResult> {
         return withContext(dispatcher) {
-            downloadMovie(apiKey)
+            val popular = async { downloadMoviePopular(apiKey) }
+            val upcoming = async { downloadMovieUpcoming(apiKey) }
+            val popularResult = popular.await()
+            val upcomingResult = upcoming.await()
+            if (popularResult is NetworkResult.Success && upcomingResult is NetworkResult.Success) {
+                NetworkResult.Success(
+                    MovieResult(
+                        popularResult.data.results + upcomingResult.data.results
+                    )
+                )
+            } else {
+                NetworkResult.Error("Error")
+            }
         }
     }
 
+    private suspend fun downloadMovieUpcoming(apiKey: String): NetworkResult<MovieResult> =
+        makeNetworkCall {
+            movieApi.getMovieUpcoming(apiKey)
+        }
 
-    private suspend fun downloadMovie(apiKey: String): NetworkResult<MovieResult> = makeNetworkCall {
-        movieApi.getMoviePopular(apiKey)
-    }
+    private suspend fun downloadMoviePopular(apiKey: String): NetworkResult<MovieResult> =
+        makeNetworkCall {
+            movieApi.getMoviePopular(apiKey)
+        }
 
 
 }
--- app/src/main/java/com/olvera/moviedbcompose/ui/home/Feed.kt
+++ app/src/main/java/com/olvera/moviedbcompose/ui/home/Feed.kt
@@ -1,21 +1,21 @@
 package com.olvera.moviedbcompose.ui.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -29,7 +29,7 @@ import com.olvera.moviedbcompose.model.Movie
 import com.olvera.moviedbcompose.util.Constants
 import kotlin.math.absoluteValue
 
-private const val GRID_SPAN_COUNT = 3
+private const val GRID_SPAN_COUNT = 2
 
 @OptIn(ExperimentalPagerApi::class)
 @ExperimentalCoilApi
@@ -41,62 +41,107 @@ fun Feed(
 ) {
     val moviePopular = viewModel.movieResponse.value
 
-    // Mostrar el carrusel de peliculas populares pero solo se muestre una sola vez
     if (moviePopular.isNotEmpty()) {
         HorizontalPager(
             count = moviePopular.size
         ) { page ->
-            Card(
-                Modifier
-                    .graphicsLayer {
-                        // Calculate the absolute offset for the current page from the
-                        // scroll position. We use the absolute value which allows us to mirror
-                        // any effects for both directions
-                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
 
-                        // We animate the scaleX + scaleY, between 85% and 100%
-                        lerp(
-                            start = 0.85f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        ).also { scale ->
-                            scaleX = scale
-                            scaleY = scale
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Card(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            // Calculate the absolute offset for the current page from the
+                            // scroll position. We use the absolute value which allows us to mirror
+                            // any effects for both directions
+                            val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                            // We animate the scaleX + scaleY, between 85% and 100%
+                            lerp(
+                                start = 0.85f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            ).also { scale ->
+                                scaleX = scale
+                                scaleY = scale
+                            }
+
+                            // We animate the alpha, between 50% and 100%
+                            alpha = lerp(
+                                start = 0.5f,
+                                stop = 1f,
+                                fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                            )
                         }
 
-                        // We animate the alpha, between 50% and 100%
-                        alpha = lerp(
-                            start = 0.5f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
-                        )
-                    }
-            ) {
+                ) {
+                    val movie = moviePopular[page]
+                    MoviePopularGridItem(movie)
 
-                val movie = moviePopular[page]
-                MovieGridItem(movie)
+
+                }
             }
         }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 250.dp, start = 8.dp, end = 8.dp, bottom = 48.dp)
+        ) {
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(GRID_SPAN_COUNT),
+                content = {
+                    items(moviePopular) { movie ->
+                        MovieUpcomingGridItem(movie)
+                    }
+                }
+            )
+        }
+
     }
 }
 
-@OptIn(ExperimentalPagerApi::class)
 @ExperimentalCoilApi
 @ExperimentalFoundationApi
 @ExperimentalMaterialApi
 @Composable
-fun MovieGridItem(
+fun MoviePopularGridItem(
     movie: Movie
 ) {
-
-    // Card content
-
-
-    Column {
+    Column(horizontalAlignment = Alignment.End) {
 
         AsyncImage(
             modifier = Modifier
-                .padding(8.dp)
+                .fillMaxWidth(),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(Constants.IMAGE_BASE_URL + Constants.IMAGE_W500 + movie.backdrop_path)
+                .crossfade(true)
+                .build(),
+            contentDescription = movie.title
+        )
+
+    }
+    movie.title?.let { Text(text = it, textAlign = TextAlign.End) }
+}
+
+@ExperimentalCoilApi
+@ExperimentalFoundationApi
+@ExperimentalMaterialApi
+@Composable
+fun MovieUpcomingGridItem(
+    movie: Movie
+) {
+    Surface(
+        modifier = Modifier
+            .padding(8.dp),
+        elevation = 6.dp,
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        AsyncImage(
+            modifier = Modifier
                 .fillMaxWidth(),
             model = ImageRequest.Builder(LocalContext.current)
                 .data(Constants.IMAGE_BASE_URL + Constants.IMAGE_W500 + movie.poster_path)
@@ -104,10 +149,39 @@ fun MovieGridItem(
                 .build(),
             contentDescription = movie.title
         )
-        movie.title?.let { Text(text = it) }
+        Box {
+            MovieInfo(
+                movie,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .background(Color(0x97000000))
+            )
+        }
 
     }
-
-
 }
+
+
+@Composable
+private fun MovieInfo(movie: Movie, modifier: Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier.padding(horizontal = 6.dp, vertical = 4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        movie.title?.let { MovieTitle(name = it) }
+    }
+}
+
+@Composable
+private fun MovieTitle(name: String) = Text(
+    text = name,
+    style = MaterialTheme.typography.subtitle1.copy(
+        color = Color.White,
+
+    ),
+    maxLines = 1,
+    overflow = TextOverflow.Ellipsis
+)
 
